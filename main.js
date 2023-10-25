e7a9b5bd2d0172646a3e1dcc798fffc62abd6df6
julia <104776492+julinhasilva@users.noreply.github.com>
2023-10-25 18:04:17 -0300

    ghj

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-function tocaSomPom(){
+function tocaSomPom() {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom').onclick =tocaSomPom;
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
-function tocasomclap(){
+function tocasomclap() {
     document.querySelector('#som_tecla_clap').play();
 }
 
-    Document.querySelector('.tecla_clap').onclick =tocasomclap;
+Document.querySelector('.tecla_clap').onclick = tocasomclap;
