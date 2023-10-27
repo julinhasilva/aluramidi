0edfe661faacabca521664a68dbdb486b7220eb9
julia <104776492+julinhasilva@users.noreply.github.com>
2023-10-27 16:02:17 -0300

    O

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-function tocaSomPom() {
+function TocaSomPom() {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+document.querySelector('.tecla_pom').onclick = TocaSomPom;
 
 function tocaSomClap() {
     document.querySelector('#som_tecla_clap').play();
 }
 
-Document.querySelector('.tecla_clap').onclick = tocaSomClap;
+Document.querySelector('.tecla_clap').onclick = TocaSomClap;
